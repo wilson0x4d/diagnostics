a405c3b1bb1661ee9a47b6f673fc374104ed53cb
wilson0x4d <mrshaunwilson@msn.com>
2018-07-16 03:17:42 -0700

    [logging] add `ConsoleTraceListener`

---
+++ X4D.Diagnostics.Test/TraceListeners/ConsoleTraceListenerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace X4D.Diagnostics.TraceListeners
+{
+    [TestClass]
+    public sealed class ConsoleTraceListenerTests
+    {
+        [TestMethod]
+        public void ConsoleTraceListener_BasicVerification()
+        {
+            var expectedText = Guid.NewGuid().ToString();
+            using (var memoryStream = new MemoryStream())
+            using (var listener = new ConsoleTraceListener(memoryStream))
+            {
+                listener.WriteLine(expectedText);
+                listener.Flush();
+                var buf = memoryStream.ToArray();
+                var actualText = Encoding.UTF8.GetString(buf, 0, buf.Length);
+                Assert.IsTrue(actualText.Contains(expectedText));
+            }
+        }
+    }
+}
---
+++ X4D.Diagnostics.TraceListeners/ConsoleTraceListener.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace X4D.Diagnostics.TraceListeners
+{
+    public class ConsoleTraceListener : 
+        TextWriterTraceListener
+    {
+        public ConsoleTraceListener()
+            : base(Console.Out)
+        {
+        }
+
+        public ConsoleTraceListener(Stream stream)
+            : base(stream)
+        {
+        }
+    }
+}
